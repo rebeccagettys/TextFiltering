b3e10a0171e5d9f5528298e72f1422b6e4a75e80
rebeccagettys <rebecca.gettys@students.olin.edu>
2016-02-23 12:18:26 -0500

    Working, just geed to graph data!

--- text_filter.py
+++ text_filter.py
@@ -18,24 +18,23 @@ def text_import():
     # Load data from a file (will be part of your data processing script)
     andersen_input_file = open('andersen_tales.pickle','r')
     andersen_tales = pickle.load(andersen_input_file)
-    #grimm_input_file = open('grimm_tales','r') #todo: add this back in later
-    #grimm_tales = pickle.load(grimm_input_file) #todo: add this back in later
+    grimm_input_file = open('grimm_tales.pickle','r')
+    grimm_tales = pickle.load(grimm_input_file)
     perrault_input_file = open('perrault_tales.pickle','r')
     perrault_tales = pickle.load(perrault_input_file)
-    return [andersen_tales,perrault_tales] #todo: add grimm_tales
+    return [andersen_tales,perrault_tales, grimm_tales]
 
 
 def color_searching(tale, initial_color_dict):
     "Searches the tale for a list of color words and counts the instances of these words up using a dictionary."
     for word in tale: #need to slice each tale into a list of words for this to work
-        print word
         color_dict = initial_color_dict
         if word in color_dict:
             current_val = color_dict.get(word)
             val = current_val + 1
             color_dict[word] = val #made a dictionary of the string (color, frequnecy)
-        print color_dict
-        return color_dict
+    return color_dict
+
 
 
 def tale_searches(talelist, initial_color_dict):
@@ -43,36 +42,52 @@ def tale_searches(talelist, initial_color_dict):
     final_dict_list = [] #empty list
     for tale in talelist:
         tale_dict=color_searching(tale, initial_color_dict)
-        final_dict_list.extend(tale_dict) #TODO:add grimm later, check if this needs to be 'append' or 'extend' - we want this to be a list of dictionaries
+        final_dict_list.append(tale_dict)
     return final_dict_list
 
-def tale_slicing(tale_list_text): #TODO: make sure that this runs on the tales BEFORE tale_searches/color_searching and add grimm tales back in
+def tale_slicing(tale_list_text):
     "Slices the tales up into a list of words without spaces # https://mail.python.org/pipermail/tutor/2001-October/009454.html explains punctuation removal "
-
+   #print len(tale_list_text)
+    tale_lists = []
     for tale in tale_list_text:
+        #print tale[0:100] #so both tales are coming in correctly so it is only going through index 1's input, ignoring index 0's
         tale_no_punc = ''
         for char in tale: #killing punctuation
             if not is_punct_char(char):
                 tale_no_punc = tale_no_punc+char #so extend the string everytime we run into a letter
         list_of_words = []
         list_of_words = tale_no_punc.split() #splitting the string into the list
-        tale_list = []
-        tale_list.extend(list_of_words)
+        #print list_of_words[0:100]
+        tale_lists.append(list_of_words)
+    return tale_lists
 
-    return tale_list
-    print tale_list
+#def tale_slices(tale_text): make sure that this runs on the tales BEFORE tale_searches/color_searching and add grimm tales back in
+#    "Slices the inputted tale up into a list of words without spaces # https://mail.python.org/pipermail/tutor/2001-October/009454.html explains punctuation removal "
+#        tale_no_punc = ''
+#        for char in tale: #killing punctuation
+#            if not is_punct_char(char):
+#                tale_no_punc = tale_no_punc+char #so extend the string everytime we run into a letter
+#        list_of_words = []
+#        list_of_words = tale_no_punc.split() #splitting the string into the list
+#        print list_of_words[0:100]
+#    return list_of_words
+
+#def tale_slicing(tale_list_text):
+#    tale_lists = []
+#    for tale in tale_list_text:
+ #       word_list = tale_slices(tale_text)
+
 
 def is_punct_char(char):
     "From python.org (link above), all this does is check if a character is puncutation or not! the ultimate helper funcion!"
     return char in string.punctuation #1 is punctuation, 0 is not punctuation
 
+
 initial_color_dict = initial_color_dict_func()
 text_lists = text_import() #this is a list of strings we need to make it a list of lists
-andersen_tales_text = text_lists[0]
-perrault_tales_text = text_lists[1]
-tale_lists= tale_slicing (text_lists)
-print tale_lists
-#output_dicts = tale_searches(text_lists, initial_color_dict) #todo: add grim tales later
-#print output_dicts
-#print type(andersen_tales)
-#color_searching(andersen_tales, initial_color_dict)
+#andersen_tales_text = text_lists[0]
+#perrault_tales_text = text_lists[1]
+#grimm_tales_text = text_lists[2]
+tale_lists = tale_slicing(text_lists)
+#tale_lists= tale_slices (text_lists) #IGNORE
+output_dicts = tale_searches(tale_lists, initial_color_dict)
