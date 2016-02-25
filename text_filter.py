922f398ae36b6f1ce731f455d1ae8b42bf12ffec
rebeccagettys <rebecca.gettys@students.olin.edu>
2016-02-24 22:02:18 -0500

    nasty bug fixed! all done :)

--- text_filter.py
+++ text_filter.py
@@ -4,22 +4,10 @@ import pickle
 import string
 import seaborn as sns
 
+COLORS = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'brown', 'grey', 'black', 'white', 'pink', 'ivory', 'tan', 'silver', 'gold', 'rose','gray', 'olive', 'crimson', 'maroon',
+    'fuchsia', 'teal', 'lavender', 'lilac', 'aqua', 'azure', 'beige', 'indigo', 'magenta', 'cyan', 'scarlet',
+    'canary', 'periwinkle']
 
-
-def initial_color_dict_func():
-    """Just the initial dictionary of colors so that we have something to start from. I used my brain, crayola crayon colors, + this link \
-    (http://www.colourlovers.com/blog/2007/07/24/32-common-color-names-for-easy-reference) to come up with color names\
-    and tried as much as possible avoid ones that could be misinterpretted (ex: plum the color vs plum the food). Because this
-     function returns things in random order, I didn't write a docstring for it, but by calling it you should have all of the colors
-      returned in the dictionary  = 0.
-
-    """
-
-    initial_color_dict = {'red': 0, 'orange': 0, 'yellow':0, 'green':0, 'blue':0, 'purple':0, 'brown':0, 'grey':0,
-    'black':0, 'white':0, 'pink':0, 'ivory':0, 'tan':0, 'silver':0, 'gold':0, 'rose':0,'gray':0, 'olive':0, 'crimson':0, 'maroon':0,
-    'fuchsia':0, 'teal':0, 'lavender':0, 'lilac':0, 'aqua':0, 'azure':0, 'beige':0, 'indigo':0, 'magenta':0, 'cyan':0, 'scarlet':0,
-    'canary':0, 'periwinkle':0}
-    return initial_color_dict #
 
 def text_import():
     """Imports previously-pickled fairy tale data (in string format from disk and returns a list of the strings.
@@ -35,13 +23,13 @@ def text_import():
     return [andersen_tales,perrault_tales, grimm_tales]
 
 
-def color_searching(tale, initial_color_dict):
+def color_searching(tale):
     """Searches the tale for a list of color words and counts the instances of these words up using a dictionary.
     Arguments: object (in this contex a list) to search, dictionary to search with
     Returns: dictionary containing keys and key-occurance frequencies (how many times the word showed up in the object)
     Due to the non-orderedness of dicionaries, hard to use a doctest"""
+    color_dict = {color:0 for color in COLORS}
     for word in tale: #need to slice each tale into a list of words for this to work
-        color_dict = initial_color_dict
         if word in color_dict:
             current_val = color_dict.get(word)
             val = current_val + 1
@@ -56,8 +44,13 @@ def tale_searches(talelist):
     Returns: list of dictionaries, one for each tale (inside each dictionary, format is color-frequency)"""
     final_dict_list = [] #empty list
     for tale in talelist:
-        tale_dict=color_searching(tale, initial_color_dict)
+        #tale_dict = {}
+        tale_dict=color_searching(tale)
+        #print type(tale_dict)
         final_dict_list.append(tale_dict)
+        # print tale_dict
+        # print final_dict_list
+    #print type(final_dict_list[0])
     return final_dict_list
 
 def tale_slicing(tale_list_text):
@@ -67,6 +60,8 @@ def tale_slicing(tale_list_text):
     Returns: list of lists of words (a list containing lists whose items are the words of each tale)"""
    #print len(tale_list_text)
     tale_lists = []
+    #print len(tale_list_text)
+
     for tale in tale_list_text:
         #print tale[0:100] #so both tales are coming in correctly so it is only going through index 1's input, ignoring index 0's
         tale_no_punc = ''
@@ -74,9 +69,11 @@ def tale_slicing(tale_list_text):
             if not is_punct_char(char):
                 tale_no_punc = tale_no_punc+char #so extend the string everytime we run into a letter
         list_of_words = []
-        list_of_words = tale_no_punc.split() #splitting the string into the list
+        list_of_words = tale_no_punc.split( ) #splitting the string into the list
+        #print type(list_of_words)
         #print list_of_words[0:100]
         tale_lists.append(list_of_words)
+    #print len(tale_lists)
     return tale_lists
 
 
@@ -87,12 +84,14 @@ def is_punct_char(char):
     Returns: True/False if the character it is given is a puncuation mark - 1 is punctuation, 0 is not """
     return char in string.punctuation #1 is punctuation, 0 is not punctuation
 
-
-initial_color_dict = initial_color_dict_func()
+# print color_searching(['red', 'the', 'blue'])
+#initial_color_dict = initial_color_dict_func()
 text_lists = text_import() #this is a list of strings we need to make it a list of lists
 #andersen, perrault, grimm is always the order
 tale_lists = tale_slicing(text_lists)
+# print color_searching(tale_lists[0])
 output_dicts = tale_searches(tale_lists)
+#print output_dicts
 
 
 # Now just a ton of data processing
@@ -101,16 +100,18 @@ andersen_dict = output_dicts[0]
 perrault_dict = output_dicts[1]
 grimm_dict = output_dicts[2]
 # dump dictionary to list of tuples
-andersen_item_dump = andersen_dict.items()
+andersen_item_dump = andersen_dict.items() #
 perrault_item_dump = perrault_dict.items()
 grimm_item_dump = grimm_dict.items()
+
+
 
 
 def list_dumping (list):
     """This method I found on #http://stackoverflow.com/questions/7558908/unpacking-a-list-tuple-of-pairs-into-two-lists-tuples;
     just a convenient snippet of code which converts from the .items output to 2 lists in correct order
     Arguments: list (of two-item-tuples) that need to be seperated into lists
-     Outputs: a list containing keys as items in one list, values as items in the other list, in the correct order"""
+     Returns: a list containing keys as items in one list, values as items in the other list, in the correct order"""
     color = []
     frequency = []
     for i in list:
@@ -125,15 +126,14 @@ grimm_color_freq = list_dumping(grimm_item_dump) #
 
 ## patrick is amazing for helping with this!!
 
-def graph_func(andersen, perrault, grimm):
+def graph_func(andersen):
     sns.set( font_scale=.8)
     sns.axlabel('Color', 'Frequency' )
     #colors from http://www.color-hex.com and wikipedia
-    flatui = ["#521515", "#fffff0", "#4b0082","#ffd700", "#7fffd4", "#e6e6fa", "#ffff00",
-              "#dc143c", "#ffc0cb", "#660000", "#808000", "#00ffff", "#d2b48c", "#c0c0c0",
-              "#ff00ff", "#0000ff", "#808080", "#ffec8b","#ab82ff", "#ee4000", "#993299",
-              "#ffaeb9", "#FF00FF", "#808080", '#f0ffff', "#008000", "#f5f5dc", "#008080",
-              "#CCCCFF","#ffa500", "#000000", "#ffffff", "#ff0000"]
+    flatui = ["#4b0082","#ffd700", "#e6e6fa", "#ffff00", "#FF2400", "#ff6eb4", "#d2b48c", "#ff00ff", "#0000ff", "#C8A2C8",
+              "#800080", "#FF007F", "#FD3F92", "#000000", "#dc143c", "#CCCCFF", "#ffffff", "#ff0000", "#631919", "#fffff0",
+              "#ffa500","#730000", "#808000","#00ffff","#c0c0c0","#808080",  "#7fffd4", "#808080", "#008000", "#f5f5dc",
+              "#329999", "#f0ffff", "#FFEF00"]
     custom_palette = sns.color_palette(flatui)
     colors = andersen[0]
     occurences = andersen[1]
@@ -143,4 +143,53 @@ def graph_func(andersen, perrault, grimm):
         item.set_rotation(45)
     sns.plt.title('Color Word Frequencies in Hans Christian Andersen Stories')
     fig.savefig('andersen_chart.png')
-graph_func(andersen_color_freq,perrault_color_freq,grimm_color_freq)
+    fig.clf()   # Clear figure
+
+
+def graph_func2(perrault):
+    sns.set( font_scale=.8)
+    sns.axlabel('Color', 'Frequency' )
+    #colors from http://www.color-hex.com and wikipedia
+    flatui = ["#4b0082","#ffd700", "#e6e6fa", "#ffff00", "#FF2400", "#ff6eb4", "#d2b48c", "#ff00ff", "#0000ff", "#C8A2C8",
+              "#800080", "#FF007F", "#FD3F92", "#000000", "#dc143c", "#CCCCFF", "#ffffff", "#ff0000", "#631919", "#fffff0",
+              "#ffa500","#730000", "#808000","#00ffff","#c0c0c0","#808080",  "#7fffd4", "#808080", "#008000", "#f5f5dc",
+              "#329999", "#f0ffff", "#FFEF00"]
+    custom_palette = sns.color_palette(flatui)
+    colors = perrault[0]
+    occurences = perrault[1]
+    ax2 = sns.barplot(colors, occurences, palette = custom_palette)
+    fig2 = ax2.get_figure()
+    for item in ax2.get_xticklabels():
+        item.set_rotation(45)
+    sns.plt.title('Color Word Frequencies in Charles Perrault Stories')
+    fig2.savefig('perrault_chart.png')
+    fig2.clf()
+
+def graph_func3(grimm):
+    sns.set( font_scale=.8)
+    sns.axlabel('Color', 'Frequency' )
+
+    #colors from http://www.color-hex.com and wikipedia
+    #flatui = ["#521515", "#fffff0", "#4b0082","#ffd700", "#7fffd4", "#e6e6fa", "#ffff00",
+    #          "#dc143c", "#ffc0cb", "#660000", "#808000", "#00ffff", "#d2b48c", "#c0c0c0",
+    #          "#ff00ff", "#0000ff", "#808080", "#ffec8b","#ab82ff", "#ee4000", "#993299",
+    #          "#ffaeb9", "#FF00FF", "#808080", '#f0ffff', "#008000", "#f5f5dc", "#008080",
+    #          "#CCCCFF","#ffa500", "#000000", "#ffffff", "#ff0000"]
+    flatui = ["#4b0082","#ffd700", "#e6e6fa", "#ffff00", "#FF2400", "#ff6eb4", "#d2b48c", "#ff00ff", "#0000ff", "#C8A2C8",
+              "#800080", "#FF007F", "#FD3F92", "#000000", "#dc143c", "#CCCCFF", "#ffffff", "#ff0000", "#631919", "#fffff0",
+              "#ffa500","#730000", "#808000","#00ffff","#c0c0c0","#808080",  "#7fffd4", "#808080", "#008000", "#f5f5dc",
+              "#329999", "#f0ffff", "#FFEF00"]
+    custom_palette = sns.color_palette(flatui)
+    colors = grimm[0]
+    occurences = grimm[1]
+    ax3 = sns.barplot(colors, occurences, palette = custom_palette)
+    fig3 = ax3.get_figure()
+    for item in ax3.get_xticklabels():
+        item.set_rotation(45)
+    sns.plt.title('Color Word Frequencies in Brothers Grimm Stories')
+    fig3.savefig('grimm_chart.png')
+    fig3.clf()
+graph_func(andersen_color_freq)
+graph_func2(perrault_color_freq)
+graph_func3(grimm_color_freq)
+#print grimm_color_freq[0]
